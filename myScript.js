7b1dfe840e94b84e7cca937f3795bed46b7e94b6
HoKeiL <hokeileung1106@gmail.com>
2023-09-28 11:44:03 +0100

    fetched episodes from TV maze api and display

--- myScript.js
+++ myScript.js
@@ -1,6 +1,6 @@
 //Modern browsers support ES5 modules with import/export as normal
 import { getElementByIdOrFail } from "./utils.js";
-import { characters } from "./characters.js";
+// import { characters } from "./characters.js";
 // import { antiHeroes } from "./antiheroes.js";
 
 const myButton = getElementByIdOrFail("myButton1");
@@ -8,29 +8,42 @@ const focusedCharacterPara = getElementByIdOrFail("focusedCharacterPara");
 const myList = getElementByIdOrFail("charactersUL");
 
 myButton.addEventListener("click", () => {
-    const searchTerm = prompt("input search term");
-    focusedCharacterPara.outerHTML = "You said: " + searchTerm;
+  const searchTerm = prompt("input search term");
+  focusedCharacterPara.outerHTML = "You said: " + searchTerm;
 });
 
+async function fetchEpisodes() {
+  const url = "https://api.tvmaze.com/shows/83/episodes";
+  const response = await fetch(url);
+  const episodes = await response.json();
+
+  return episodes;
+}
+let episodes = await fetchEpisodes();
+
 function makeLiElementsForCharacters() {
-    return characters.map((character) => {
-        //Not yet attached to any point in the DOM tree
-        const element = document.createElement("li");
-        element.innerHTML = character.name + " from " + character.book;
+  return episodes.map((episode) => {
+    //Not yet attached to any point in the DOM tree
+    const element = document.createElement("li");
+    element.innerHTML =
+      episode.name +
+      " from  season" +
+      episode.season +
+      " episode " +
+      episode.number;
 
-        element.addEventListener("click", () => {
-            alert(character.powers.join(", "));
-        });
-        element.addEventListener("mouseover", () => {
-            focusedCharacterPara.innerText =
-                character.name + ": " + character.powers.join(", ");
-        });
-
-        return element;
+    element.addEventListener("click", () => {
+      alert(episode.summary);
     });
+    element.addEventListener("mouseover", () => {
+      focusedCharacterPara.innerText = episode.name + ": " + episode.summary;
+    });
+
+    return element;
+  });
 }
 const characterLiElements = makeLiElementsForCharacters();
 
 for (const li of characterLiElements) {
-    myList.appendChild(li);
+  myList.appendChild(li);
 }
